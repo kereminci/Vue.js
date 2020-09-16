54ee9a452eb12a2a30ee97aafdb527a204ac937c
kereminci <54918783+kereminci@users.noreply.github.com>
2020-09-17 00:36:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+el: '#app',
+data: {
+  name: 'Max',
+  elements:[]
+ 
+},
+methods: {
+  changeName: function(){
+    this.name = 'Anna';
+  },
+  addElement: function(){
+    this.elements.push(this.elements.length + 1);
+  }
+  
+}
+});
